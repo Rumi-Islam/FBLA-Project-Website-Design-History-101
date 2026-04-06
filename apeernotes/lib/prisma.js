6541d408397f4ccc79aed70aee1063068898b480
Rumi Islam <rumi16625@gmail.com>
2026-04-06 14:36:48 +0000

    tno building

--- apeernotes/lib/prisma.js
+++ apeernotes/lib/prisma.js
@@ -10,4 +10,4 @@ const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 export default prisma
 
-if (process.env.NODE_NODE !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
